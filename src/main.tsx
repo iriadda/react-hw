6bdfde27fd2f072ccf59ebd8cac886724aabdaad
iriadda <iriadda@gmail.com>
2025-01-13 15:18:43 +0200

    hw 3 task 3 update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from "react-router";
-import {routes} from "./routes/routes.ts";
+import {routes} from "./routes/routes.tsx";
 
 
 createRoot(document.getElementById('root')!).render(<RouterProvider router={routes}/>)
